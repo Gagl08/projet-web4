7b6733bc09e5109dc2255bb0204efcf2ca7feb5c
Lucàs <vabrelucas@gmail.com>
2023-04-11 22:01:24 +0200

    fix: edit protected path from login
    Took 2 minutes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,10 @@
-export { default } from "next-auth/middleware"
+export {default} from 'next-auth/middleware';
 
 export const config = {
   matcher: [
-      "/dashboard",
-      "/profile",
-      '/chat/:path*'
-  ]
-}
+    '/dashboard',
+    '/userProfile',
+    '/chat',
+    '/chat/*',
+  ],
+};
