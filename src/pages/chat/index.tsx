20e9c9bd0edc5aa81632f9df55f12c93853e5149
Lucàs <vabrelucas@gmail.com>
2023-05-05 20:49:41 +0200

    refactor: Chat
    Took 2 minutes

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -1,17 +1,20 @@
-import {Button, Text} from '@chakra-ui/react';
+import {Button} from '@chakra-ui/react';
 import {useSession} from 'next-auth/react';
 import {useRouter} from 'next/router';
 
 export default function Chat() {
   const router = useRouter();
-  const {data: session, status} = useSession()
+  const {data: session, status} = useSession();
 
   if (session)
-  return (
-      <>
-        {
-          session.user.ChatID.map((id: string, index: number) => <Button key={index} onClick={() => router.push(`/chat/${id}`)} >{id}</Button>)
-        }
-      </>
-  )
+    return (
+        <>
+          {session.user.ChatID.map(
+              (id: string, index: number) => (
+                  <Button key={index} onClick={() =>
+                      router.push(`/chat/${id}`)}>{id}</Button>
+              ),
+          )}
+        </>
+    );
 }
